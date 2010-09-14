9c8c442b3d31d62cd12a81124c382e61556c46f9
Thomas Risberg <trisberg@vmware.com>
2010-09-14 13:07:25 -0400

    updated to use parent pom; added query methods

--- src/main/java/org/springframework/datastore/document/mongodb/MongoTemplate.java
+++ src/main/java/org/springframework/datastore/document/mongodb/MongoTemplate.java
@@ -25,7 +25,10 @@ import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.datastore.document.AbstractDocumentStoreTemplate;
 import org.springframework.datastore.document.DocumentMapper;
 import org.springframework.datastore.document.DocumentSource;
+import org.springframework.datastore.document.InvalidDocumentStoreApiUageException;
+import org.springframework.util.Assert;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.CommandResult;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -106,6 +109,30 @@ public class MongoTemplate extends AbstractDocumentStoreTemplate<DB> {
 		return results;
 	}
 	
+	public <T> List<T> queryForList(String collectionName, String fieldName, String[] operands, Object[] values, Class<T> targetClass) {
+		DocumentMapper<DBObject, T> mapper = MongoBeanPropertyDocumentMapper.newInstance(targetClass);
+		return queryForList(collectionName, fieldName, operands, values, mapper);
+	}
+
+	public <T> List<T> queryForList(String collectionName, String fieldName, String[] operands, Object[] values, DocumentMapper<DBObject, T> mapper) {
+		if (operands.length != values.length) {
+			throw new InvalidDocumentStoreApiUageException("The number of operands and values must match");
+		}
+		List<T> results = new ArrayList<T>();
+		DBObject query = new BasicDBObject();
+		DBObject condition = new BasicDBObject();
+		for (int i = 0; i < operands.length; i++) {
+			condition.put(operands[i], values[i]);
+		}
+		query.put(fieldName, condition);
+		System.out.println("--> " + query);
+		DBCollection collection = getConnection().getCollection(collectionName);
+		for (DBObject dbo : collection.find(query)) {
+			results.add(mapper.mapDocument(dbo));
+		}
+		return results;
+	}
+
 	public RuntimeException translateIfNecessary(RuntimeException ex) {
 		return MongoDbUtils.translateMongoExceptionIfPossible(ex);
 	}
