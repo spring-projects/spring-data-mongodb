38ca06bd4903b33a2d1f8535da40e68e2b5ab7c2
Thomas Risberg <trisberg@vmware.com>
2010-12-03 09:33:27 -0500

    DATADOC-23 removed DocumentSource and its subclasses

--- spring-data-couchdb/src/main/java/org/springframework/data/document/couchdb/CouchDocumentSource.java
+++
@@ -1,8 +0,0 @@
-package org.springframework.data.document.couchdb;
-
-import org.jcouchdb.document.BaseDocument;
-import org.springframework.data.document.DocumentSource;
-
-public interface CouchDocumentSource extends DocumentSource<BaseDocument> {
-
-}
