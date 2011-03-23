1183789bb3f377ebe4bbbf6583cdc2fcca9ccba7
Oliver Gierke <info@olivergierke.de>
2011-03-23 09:12:07 +0100

    DATADOC-69 - Repository namespace now allows referenceing a MappingContext and picks up the default one if available.

--- spring-data-mongodb/src/test/java/org/springframework/data/document/mongodb/repository/config/MongoNamespaceIntegrationTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/document/mongodb/repository/config/MongoNamespaceIntegrationTests.java
@@ -1,5 +1,12 @@
 package org.springframework.data.document.mongodb.repository.config;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.document.mongodb.repository.AbstractPersonRepositoryIntegrationTests;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -14,4 +21,13 @@ import org.springframework.test.context.ContextConfiguration;
 public class MongoNamespaceIntegrationTests extends
     AbstractPersonRepositoryIntegrationTests {
 
+  
+  @Test
+  public void assertDefaultMappingContextIsWired() {
+    
+    XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("MongoNamespaceIntegrationTests-context.xml", getClass()));
+    BeanDefinition definition = factory.getBeanDefinition("personRepository");
+    assertThat(definition, is(notNullValue()));
+    assertThat(definition.getPropertyValues().getPropertyValue("mappingContext"), is(notNullValue()));
+  }
 }
