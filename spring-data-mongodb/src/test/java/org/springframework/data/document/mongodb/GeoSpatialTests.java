db45349553a8c61dc13dbbb7a7db0b6db7461d85
Thomas Risberg <trisberg@vmware.com>
2011-03-31 23:01:01 -0400

    fixed coordinates for Penn Station

--- spring-data-mongodb/src/test/java/org/springframework/data/document/mongodb/GeoSpatialTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/document/mongodb/GeoSpatialTests.java
@@ -55,7 +55,7 @@ public class GeoSpatialTests {
 
   private void addVenues() {
     // Data taken from https://github.com/deftlabs/mongo-java-geospatial-example
-    template.insert(new Venue("Penn Station", 73.99408, 40.75057));
+    template.insert(new Venue("Penn Station", -73.99408, 40.75057));
     template.insert(new Venue("10gen Office", -73.99171, 40.738868));
     template.insert(new Venue("Flatiron Building", -73.988135, 40.741404));
     template.insert(new Venue("Players Club", -73.997812, 40.739128));
