c7c2a66c3ba88165234fdfc10dea428fc2fc7281
Mark Pollack <mark.pollack@springsource.com>
2011-05-24 17:24:08 -0400

    DATADOC-88 - Create MongoDbFactory to consolidate DB, Server location, and user credentials into one location

--- spring-data-mongodb/src/main/java/org/springframework/data/document/mongodb/MongoDbFactory.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/document/mongodb/MongoDbFactory.java
@@ -3,7 +3,6 @@ package org.springframework.data.document.mongodb;
 import org.springframework.dao.DataAccessException;
 
 import com.mongodb.DB;
-import com.mongodb.Mongo;
 
 public interface MongoDbFactory {
 
