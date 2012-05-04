6aba521be69bc3eedf57441c9318f8b2557cde7e
Maciej Walkowiak <maciej.walkowiak@young-internet.com>
2012-05-04 19:15:37 +0200

    Added javadocs

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/mapping/CascadeType.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/mapping/CascadeType.java
@@ -1,9 +1,29 @@
 package org.springframework.data.mongodb.core.mapping;
 
+/**
+ * Used to define cascade type in {@link DBRef} annotation
+ *
+ * @author Maciej Walkowiak
+ */
 public enum CascadeType {
+	/**
+	 * Includes {@link CascadeType#SAVE} amd {@link CascadeType#DELETE}
+	 */
 	ALL,
+
+	/**
+	 * Cascades save operation
+	 */
 	SAVE,
+
+	/**
+	 * Cascades delete operation
+	 */
 	DELETE,
+
+	/**
+	 * Default option. No operation is cascaded
+	 */
 	NONE;
 
 	public boolean isSave() {
