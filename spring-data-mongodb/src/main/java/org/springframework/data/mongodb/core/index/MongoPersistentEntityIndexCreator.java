1380c49fd0e3473c8866a983a26c817d6f512a71
Oliver Gierke <info@olivergierke.de>
2012-07-16 18:22:11 +0200

    DATAMONGO-483 - Indexes now use the field name even if index name is defined.

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/index/MongoPersistentEntityIndexCreator.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/index/MongoPersistentEntityIndexCreator.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2011 by the original author(s).
+ * Copyright 2011-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.springframework.data.mongodb.core.index;
 
 import java.lang.reflect.Field;
@@ -27,7 +26,6 @@ import org.springframework.data.mapping.PersistentEntity;
 import org.springframework.data.mapping.PropertyHandler;
 import org.springframework.data.mapping.event.MappingContextEvent;
 import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.mapping.BasicMongoPersistentEntity;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.core.mapping.MongoPersistentEntity;
 import org.springframework.data.mongodb.core.mapping.MongoPersistentProperty;
@@ -39,10 +37,10 @@ import com.mongodb.DBObject;
 import com.mongodb.util.JSON;
 
 /**
- * Component that inspects {@link BasicMongoPersistentEntity} instances contained in the given
- * {@link MongoMappingContext} for indexing metadata and ensures the indexes to be available.
+ * Component that inspects {@link MongoPersistentEntity} instances contained in the given {@link MongoMappingContext}
+ * for indexing metadata and ensures the indexes to be available.
  * 
- * @author Jon Brisbin <jbrisbin@vmware.com>
+ * @author Jon Brisbin
  * @author Oliver Gierke
  */
 public class MongoPersistentEntityIndexCreator implements
@@ -97,12 +95,12 @@ public class MongoPersistentEntityIndexCreator implements
 			if (type.isAnnotationPresent(CompoundIndexes.class)) {
 				CompoundIndexes indexes = type.getAnnotation(CompoundIndexes.class);
 				for (CompoundIndex index : indexes.value()) {
-					String indexColl = index.collection();
-					if ("".equals(indexColl)) {
-						indexColl = entity.getCollection();
-					}
-					ensureIndex(indexColl, index.name(), index.def(), index.direction(), index.unique(), index.dropDups(),
-							index.sparse());
+
+					String indexColl = StringUtils.hasText(index.collection()) ? index.collection() : entity.getCollection();
+					DBObject definition = (DBObject) JSON.parse(index.def());
+
+					ensureIndex(indexColl, index.name(), definition, index.unique(), index.dropDups(), index.sparse());
+
 					if (log.isDebugEnabled()) {
 						log.debug("Created compound index " + index);
 					}
@@ -111,10 +109,14 @@ public class MongoPersistentEntityIndexCreator implements
 
 			entity.doWithProperties(new PropertyHandler<MongoPersistentProperty>() {
 				public void doWithPersistentProperty(MongoPersistentProperty persistentProperty) {
+
 					Field field = persistentProperty.getField();
+
 					if (field.isAnnotationPresent(Indexed.class)) {
+
 						Indexed index = field.getAnnotation(Indexed.class);
 						String name = index.name();
+
 						if (!StringUtils.hasText(name)) {
 							name = persistentProperty.getFieldName();
 						} else {
@@ -126,11 +128,17 @@ public class MongoPersistentEntityIndexCreator implements
 								}
 							}
 						}
+
 						String collection = StringUtils.hasText(index.collection()) ? index.collection() : entity.getCollection();
-						ensureIndex(collection, name, null, index.direction(), index.unique(), index.dropDups(), index.sparse());
+						int direction = index.direction() == IndexDirection.ASCENDING ? 1 : -1;
+						DBObject definition = new BasicDBObject(persistentProperty.getFieldName(), direction);
+
+						ensureIndex(collection, name, definition, index.unique(), index.dropDups(), index.sparse());
+
 						if (log.isDebugEnabled()) {
 							log.debug("Created property index " + index);
 						}
+
 					} else if (field.isAnnotationPresent(GeoSpatialIndexed.class)) {
 
 						GeoSpatialIndexed index = field.getAnnotation(GeoSpatialIndexed.class);
@@ -155,21 +163,15 @@ public class MongoPersistentEntityIndexCreator implements
 		}
 	}
 
-	protected void ensureIndex(String collection, final String name, final String def, final IndexDirection direction,
-			final boolean unique, final boolean dropDups, final boolean sparse) {
-		DBObject defObj;
-		if (null != def) {
-			defObj = (DBObject) JSON.parse(def);
-		} else {
-			defObj = new BasicDBObject();
-			defObj.put(name, (direction == IndexDirection.ASCENDING ? 1 : -1));
-		}
+	protected void ensureIndex(String collection, String name, DBObject indexDefinition, boolean unique,
+			boolean dropDups, boolean sparse) {
+
 		DBObject opts = new BasicDBObject();
 		opts.put("name", name);
 		opts.put("dropDups", dropDups);
 		opts.put("sparse", sparse);
 		opts.put("unique", unique);
-		mongoDbFactory.getDb().getCollection(collection).ensureIndex(defObj, opts);
-	}
 
+		mongoDbFactory.getDb().getCollection(collection).ensureIndex(indexDefinition, opts);
+	}
 }
---
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/index/MongoPersistentEntityIndexCreatorUnitTests.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.mongodb.core.index;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.Collections;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+import com.mongodb.DBObject;
+
+/**
+ * Unit tests for {@link MongoPersistentEntityIndexCreator}.
+ * 
+ * @author Oliver Gierke
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class MongoPersistentEntityIndexCreatorUnitTests {
+
+	@Mock
+	MongoDbFactory factory;
+
+	@Test
+	public void buildsIndexDefinitionUsingFieldName() {
+
+		MongoMappingContext mappingContext = new MongoMappingContext();
+		mappingContext.setInitialEntitySet(Collections.singleton(Person.class));
+		mappingContext.initialize();
+
+		DummyMongoPersistentEntityIndexCreator creator = new DummyMongoPersistentEntityIndexCreator(mappingContext, factory);
+
+		assertThat(creator.indexDefinition, is(notNullValue()));
+		assertThat(creator.indexDefinition.keySet(), hasItem("fieldname"));
+		assertThat(creator.name, is("indexName"));
+	}
+
+	static class Person {
+
+		@Indexed(name = "indexName")
+		@Field("fieldname")
+		String field;
+	}
+
+	static class DummyMongoPersistentEntityIndexCreator extends MongoPersistentEntityIndexCreator {
+
+		DBObject indexDefinition;
+		String name;
+
+		public DummyMongoPersistentEntityIndexCreator(MongoMappingContext mappingContext, MongoDbFactory mongoDbFactory) {
+			super(mappingContext, mongoDbFactory);
+		}
+
+		@Override
+		protected void ensureIndex(String collection, String name, DBObject indexDefinition, boolean unique,
+				boolean dropDups, boolean sparse) {
+
+			this.name = name;
+			this.indexDefinition = indexDefinition;
+		}
+	}
+}
