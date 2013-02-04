3e058c0d5d672f2c108022c55f1b1cbc41d84665
Tobias Trelle <tobias.trelle@codecentric.de>
2013-02-04 08:26:49 +0100

    Added type safe versions for aggregation operations $match and $sort

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/aggregation/AggregationPipeline.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/aggregation/AggregationPipeline.java
@@ -18,6 +18,8 @@ package org.springframework.data.mongodb.core.aggregation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.util.Assert;
 
 import com.mongodb.BasicDBObject;
@@ -36,6 +38,7 @@ public class AggregationPipeline {
 
 	/**
 	 * Adds a projection operation to the pipeline.
+	 * 
 	 * @param projection JSON string holding the projection.
 	 * @return The pipeline.
 	 */
@@ -45,76 +48,110 @@ public class AggregationPipeline {
 
 	/**
 	 * Adds an unwind operation to the pipeline.
+	 * 
 	 * @param field Name of the field to unwind (should be an array).
 	 * @return The pipeline.
 	 */
 	public AggregationPipeline unwind(String field) {
 		Assert.notNull(field, "Missing field name");
-		addOperation( new BasicDBObject("$unwind", field) );
+		addOperation("unwind", field);
 		return this;
 	}
 
 	/**
 	 * Adds a group operation to the pipeline.
+	 * 
 	 * @param projection JSON string holding the group.
 	 * @return The pipeline.
 	 */
 	public AggregationPipeline group(String group) {
 		return addDocumentOperation("group", group);
 	}
-	
+
 	/**
 	 * Adds a sort operation to the pipeline.
-	 * @param projection JSON string holding the group.
+	 * 
+	 * @param sort JSON string holding the sorting.
 	 * @return The pipeline.
 	 */
 	public AggregationPipeline sort(String sort) {
 		return addDocumentOperation("sort", sort);
-	}	
-	
+	}
+
+	/**
+	 * Adds a sort operation to the pipeline.
+	 * 
+	 * @param sort Type safe sort operation.
+	 * @return The pipeline.
+	 */
+	public AggregationPipeline sort(Sort sort) {
+		DBObject dbo = new BasicDBObject();
+
+		for (org.springframework.data.domain.Sort.Order order : sort) {
+			dbo.put(order.getProperty(), order.isAscending() ? 1 : -1);
+		}
+		addOperation("sort", dbo);
+		
+		return this;
+	}
+
 	/**
 	 * Adds a match operation to the pipeline that is basically a query on the collection.s
-	 * @param projection JSON string holding the match.
+	 * 
+	 * @param projection JSON string holding the criteria.
 	 * @return The pipeline.
 	 */
 	public AggregationPipeline match(String match) {
-		return addDocumentOperation("match", match);	
-	}		
-	
+		return addDocumentOperation("match", match);
+	}
+
+	/**
+	 * Adds a match operation to the pipeline that is basically a query on the collection.s
+	 * 
+	 * @param criteria Type safe criteria to filter documents from the collection.
+	 * @return The pipeline.
+	 */
+	public AggregationPipeline match(Criteria criteria) {
+		addOperation("match", criteria.getCriteriaObject());
+		return this;
+	}
+
 	/**
 	 * Adds an limit operation to the pipeline.
+	 * 
 	 * @param n Number of document to consider.
 	 * @return The pipeline.
 	 */
 	public AggregationPipeline limit(long n) {
-		addOperation( new BasicDBObject("$limit", n) );
+		addOperation("limit", n);
 		return this;
 	}
-	
+
 	/**
 	 * Adds an skip operation to the pipeline.
+	 * 
 	 * @param n Number of documents to skip.
 	 * @return The pipeline.
 	 */
 	public AggregationPipeline skip(long n) {
-		addOperation( new BasicDBObject("$skip", n) );
+		addOperation("skip", n);
 		return this;
-	}	
-	
+	}
+
 	public List<DBObject> getOperations() {
 		return operations;
 	}
 
 	private AggregationPipeline addDocumentOperation(String opName, String operation) {
 		Assert.notNull(operation, "Missing " + opName);
-		addOperation( new BasicDBObject("$" + opName, parseJson(operation)) );	
+		addOperation(opName, parseJson(operation));
 		return this;
 	}
-	
-	private void addOperation(DBObject op) {
-		this.operations.add(op);
+
+	private void addOperation(String key, Object value) {
+		this.operations.add(new BasicDBObject("$" + key, value));
 	}
-	
+
 	private DBObject parseJson(String json) {
 		try {
 			return (DBObject) JSON.parse(json);
