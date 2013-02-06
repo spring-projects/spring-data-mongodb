df48c6d48b9e6ca848f84a9cfffc98ae64f48758
Tobias Trelle <tobias.trelle@codecentric.de>
2013-02-06 09:16:04 +0100

    support for $add and $substract in projection

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/aggregation/ProjectionTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/aggregation/ProjectionTests.java
@@ -4,6 +4,8 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
@@ -19,94 +21,143 @@ public class ProjectionTests {
 
 	/** Unit under test. */
 	private Projection projection;
-	
-	@Before public void setUp() {
+
+	@Before
+	public void setUp() {
 		projection = new Projection();
 	}
-	
-	@Test public void emptyProjection() {
+
+	@Test
+	public void emptyProjection() {
 		// when
 		DBObject raw = projection.toDBObject();
-		
+
 		// then
-		assertThat( raw, notNullValue() );
-		assertThat( raw.toMap().isEmpty(), is(true) );
+		assertThat(raw, notNullValue());
+		assertThat(raw.toMap().isEmpty(), is(true));
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldDetectNullIncludesInConstructor() {
 		// when
-		new Projection((String[])null);
+		new Projection((String[]) null);
 		// then: throw expected exception
 	}
-	
-	@Test public void includesWithConstructor() {
+
+	@Test
+	public void includesWithConstructor() {
 		// given
 		projection = new Projection("a", "b");
-		
+
 		// when
 		DBObject raw = projection.toDBObject();
-		
+
 		// then
-		assertThat( raw, notNullValue() );
-		assertThat( raw.toMap().size(), is(3) );
-		assertThat( (Integer)raw.get("_id"), is(0) );
-		assertThat( (Integer)raw.get("a"), is(1) );
-		assertThat( (Integer)raw.get("b"), is(1) );
+		assertThat(raw, notNullValue());
+		assertThat(raw.toMap().size(), is(3));
+		assertThat((Integer) raw.get("_id"), is(0));
+		assertThat((Integer) raw.get("a"), is(1));
+		assertThat((Integer) raw.get("b"), is(1));
 	}
-	
-	@Test public void include() {
+
+	@Test
+	public void include() {
 		// given
 		projection.include("a");
-		
+
 		// when
 		DBObject raw = projection.toDBObject();
-		
+
 		// then
 		assertSingleDBObject("a", 1, raw);
 	}
 
-	@Test public void exclude() {
+	@Test
+	public void exclude() {
 		// given
 		projection.exclude("a");
-		
+
 		// when
 		DBObject raw = projection.toDBObject();
-		
+
 		// then
 		assertSingleDBObject("a", 0, raw);
 	}
-	
-	@Test public void includeAlias() {
+
+	@Test
+	public void includeAlias() {
 		// given
 		projection.include("a").as("b");
-		
+
 		// when
 		DBObject raw = projection.toDBObject();
-		
+
 		// then
 		assertSingleDBObject("b", "$a", raw);
 	}
 
-	@Test(expected = InvalidDataAccessApiUsageException.class ) 
+	@Test(expected = InvalidDataAccessApiUsageException.class)
 	public void shouldDetectAliasWithoutInclude() {
 		// when
 		projection.as("b");
 		// then: throw expected exception
 	}
 
-	@Test(expected = InvalidDataAccessApiUsageException.class ) 
+	@Test(expected = InvalidDataAccessApiUsageException.class)
 	public void shouldDetectDuplicateAlias() {
 		// when
 		projection.include("a").as("b").as("c");
 		// then: throw expected exception
 	}
+
+	@Test
+	public void plus() {
+		// given
+		projection.include("a").plus(10);
+
+		// when
+		DBObject raw = projection.toDBObject();
+
+		// then
+		assertNotNullDBObject(raw);
+		DBObject addition = (DBObject)raw.get("a");
+		assertNotNullDBObject(addition);	
+		@SuppressWarnings("unchecked")
+		List<Object> summands = (List<Object>)addition.get("$add");
+		assertThat( summands, notNullValue() );
+		assertThat( summands.size(), is(2) );
+		assertThat( (String)summands.get(0), is("$a") );
+		assertThat( (Integer)summands.get(1), is (10) );
+	}
+
+	@Test
+	public void plusWithAlias() {
+		// given
+		projection.include("a").plus(10).as("b");
+
+		// when
+		DBObject raw = projection.toDBObject();
+
+		// then
+		assertNotNullDBObject(raw);
+		DBObject addition = (DBObject)raw.get("b");
+		assertNotNullDBObject(addition);	
+		@SuppressWarnings("unchecked")
+		List<Object> summands = (List<Object>)addition.get("$add");
+		assertThat( summands, notNullValue() );
+		assertThat( summands.size(), is(2) );
+		assertThat( (String)summands.get(0), is("$a") );
+		assertThat( (Integer)summands.get(1), is (10) );
+	}
 	
 	
 	private static void assertSingleDBObject(String key, Object value, DBObject doc) {
-		assertThat( doc, notNullValue() );
-		assertThat( doc.toMap().size(), is(1) );		
-		assertThat( doc.get(key), is(value) );		
-	}	
-	
+		assertNotNullDBObject(doc);
+		assertThat(doc.get(key), is(value));
+	}
+
+	private static void assertNotNullDBObject(DBObject doc) {
+		assertThat(doc, notNullValue());
+		assertThat(doc.toMap().size(), is(1));
+	}
 }
