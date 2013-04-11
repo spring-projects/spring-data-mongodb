4d65aa72073ccf5c84a2f79dc4215facfe21fcc1
Oliver Gierke <ogierke@vmware.com>
2013-04-11 10:48:52 +0200

    DATAMONGO-586 - First round of polish.
    Fixed or added copyright headers where necessary. Added Tobias as author where necessary. Added @since tags to newly introduced classes and methods. Documented non-nullability of parameters. Polished test cases a bit.

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/aggregation/AggregationPipelineTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/aggregation/AggregationPipelineTests.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.mongodb.core.aggregation;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.springframework.data.mongodb.core.DBObjectUtils.*;
+
 import java.util.List;
 
 import org.junit.Before;
@@ -16,122 +32,112 @@ import com.mongodb.DBObject;
 /**
  * Tests of the {@link AggregationPipeline}.
  * 
+ * @see DATAMONGO-586
  * @author Tobias Trelle
  */
 public class AggregationPipelineTests {
 
-	/** Unit under test. */
-	private AggregationPipeline pipeline;
-	
-	@Before public void setUp() {
+	AggregationPipeline pipeline;
+
+	@Before
+	public void setUp() {
 		pipeline = new AggregationPipeline();
 	}
-	
-	@Test public void limitOperation() {
-		// given
+
+	@Test
+	public void limitOperation() {
+
 		pipeline.limit(42);
-		
-		// when
+
 		List<DBObject> rawPipeline = pipeline.getOperations();
-		
-		// then
 		assertDBObject("$limit", 42L, rawPipeline);
 	}
 
-	@Test public void skipOperation() {
-		// given
+	@Test
+	public void skipOperation() {
+
 		pipeline.skip(5);
-		
-		// when
+
 		List<DBObject> rawPipeline = pipeline.getOperations();
-		
-		// then
 		assertDBObject("$skip", 5L, rawPipeline);
 	}
 
-	@Test public void unwindOperation() {
-		// given
+	@Test
+	public void unwindOperation() {
+
 		pipeline.unwind("$field");
-		
-		// when
+
 		List<DBObject> rawPipeline = pipeline.getOperations();
-		
-		// then
 		assertDBObject("$unwind", "$field", rawPipeline);
 	}
 
-	@Test public void unwindOperationWithAddedPrefix() {
-		// given
+	@Test
+	public void unwindOperationWithAddedPrefix() {
+
 		pipeline.unwind("field");
-		
-		// when
+
 		List<DBObject> rawPipeline = pipeline.getOperations();
-		
-		// then
 		assertDBObject("$unwind", "$field", rawPipeline);
 	}
-	
-	
-	@Test public void matchOperation() {
-		// given
+
+	@Test
+	public void matchOperation() {
+
 		Criteria criteria = new Criteria("title").is("Doc 1");
-		pipeline.match( criteria );
-		
-		// when
+		pipeline.match(criteria);
+
 		List<DBObject> rawPipeline = pipeline.getOperations();
-		
-		// then
 		assertOneDocument(rawPipeline);
+
 		DBObject match = rawPipeline.get(0);
-		DBObject criteriaDoc = (DBObject)match.get("$match");
-		assertThat( criteriaDoc, notNullValue() );
-		assertSingleDBObject( "title" , "Doc 1", criteriaDoc );
+		DBObject criteriaDoc = getAsDBObject(match, "$match");
+		assertThat(criteriaDoc, is(notNullValue()));
+		assertSingleDBObject("title", "Doc 1", criteriaDoc);
 	}
 
-	@Test public void sortOperation() {
-		// given
+	@Test
+	public void sortOperation() {
+
 		Sort sort = new Sort(new Sort.Order(Direction.ASC, "n"));
-		pipeline.sort( sort );
-		
-		// when
+		pipeline.sort(sort);
+
 		List<DBObject> rawPipeline = pipeline.getOperations();
-		
-		// then
 		assertOneDocument(rawPipeline);
+
 		DBObject sortDoc = rawPipeline.get(0);
-		DBObject orderDoc = (DBObject)sortDoc.get("$sort");
-		assertThat( orderDoc, notNullValue() );
-		assertSingleDBObject( "n" , 1, orderDoc );
+		DBObject orderDoc = getAsDBObject(sortDoc, "$sort");
+		assertThat(orderDoc, is(notNullValue()));
+		assertSingleDBObject("n", 1, orderDoc);
 	}
-	
-	@Test public void projectOperation() {
-		// given
+
+	@Test
+	public void projectOperation() {
+
 		Projection projection = new Projection("a");
 		pipeline.project(projection);
-		
-		// when
+
 		List<DBObject> rawPipeline = pipeline.getOperations();
-		
-		// then
 		assertOneDocument(rawPipeline);
+
 		DBObject projectionDoc = rawPipeline.get(0);
-		DBObject fields = (DBObject)projectionDoc.get("$project");
-		assertThat( fields, notNullValue() );
-		assertSingleDBObject( "a" , 1, fields );
+		DBObject fields = getAsDBObject(projectionDoc, "$project");
+		assertThat(fields, is(notNullValue()));
+		assertSingleDBObject("a", 1, fields);
 	}
-	
+
 	private static void assertOneDocument(List<DBObject> result) {
-		assertThat( result, notNullValue() );
-		assertThat( result.size(), is(1) );		
+
+		assertThat(result, is(notNullValue()));
+		assertThat(result.size(), is(1));
 	}
-	
+
 	private static void assertDBObject(String key, Object value, List<DBObject> result) {
+
 		assertOneDocument(result);
-		assertSingleDBObject( key, value, result.get(0) );		
+		assertSingleDBObject(key, value, result.get(0));
 	}
 
 	private static void assertSingleDBObject(String key, Object value, DBObject doc) {
-		assertThat( doc.get(key), is(value) );		
+		assertThat(doc.get(key), is(value));
 	}
-	
 }
