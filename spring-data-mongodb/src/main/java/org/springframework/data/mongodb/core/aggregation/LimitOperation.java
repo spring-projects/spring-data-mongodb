cf612eca33001f8e51b55ed45f0c5d41350fe78f
Thomas Darimont <tdarimont@gopivotal.com>
2014-08-12 12:13:30 +0200

    DATAMONGO-1020 - Make LimitOperation public.
    Previously LimitOperator is not visible and thus not useably.
    
    Original pull request: #218.

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/aggregation/LimitOperation.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/aggregation/LimitOperation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 the original author or authors.
+ * Copyright 2013-2014 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import com.mongodb.DBObject;
  * @author Oliver Gierke
  * @since 1.3
  */
-class LimitOperation implements AggregationOperation {
+public class LimitOperation implements AggregationOperation {
 
 	private final long maxElements;
 
