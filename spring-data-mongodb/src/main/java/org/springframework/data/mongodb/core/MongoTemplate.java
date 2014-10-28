5c5accd12d806e3a21e27359db0d6ffff992cf8b
Christoph Strobl <cstrobl@pivotal.io>
2014-10-28 12:54:49 +0100

    DATAMONGO-1078 - @Query annotated repository method fails for complex Id when used with Collection type.
    Remove object type hint defaulting.

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/MongoTemplate.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/MongoTemplate.java
@@ -1606,7 +1606,7 @@ public class MongoTemplate implements MongoOperations, ApplicationContextAware {
 
 		if (LOGGER.isDebugEnabled()) {
 			LOGGER.debug(String.format("find using query: %s fields: %s for class: %s in collection: %s",
-					serializeToJsonSafely(query), mappedFields, entityClass, collectionName));
+					serializeToJsonSafely(mappedQuery), mappedFields, entityClass, collectionName));
 		}
 
 		return executeFindMultiInternal(new FindCallback(mappedQuery, mappedFields), preparer, objectCallback,
