a13e7b8b2421aaf79eaa3b3eab05d1ed09b29088
Oliver Gierke <ogierke@pivotal.io>
2015-11-26 11:56:27 +0100

    DATAMONGO-1337 - Reverted making some of the loggers static.
    The logger instance in AbstractMonitor is supposed to pick up the type of the actual implementation class and thus cannot be static.
    
    Related pull request: #336.

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/monitor/AbstractMonitor.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/monitor/AbstractMonitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2012 the original author or authors.
+ * Copyright 2002-2015 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,10 +29,11 @@ import com.mongodb.MongoException;
  * Base class to encapsulate common configuration settings when connecting to a database
  * 
  * @author Mark Pollack
+ * @author Oliver Gierke
  */
 public abstract class AbstractMonitor {
 
-	private static final Logger logger = LoggerFactory.getLogger(getClass());
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	protected Mongo mongo;
 	private String username;
