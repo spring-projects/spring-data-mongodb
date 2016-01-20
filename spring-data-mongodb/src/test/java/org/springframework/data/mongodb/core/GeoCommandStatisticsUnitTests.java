294990891ce68e2ad49d3253a7a603edd5da5956
Oliver Gierke <ogierke@pivotal.io>
2016-01-20 16:09:35 +0100

    DATAMONGO-1361 - Guard command result statistics evaluation against changes in MongoDB 3.2.
    MongoDB 3.2 RC1 decided to remove fields from statistics JSON documents returned in case no result was found for a geo near query. The avgDistance field is unfortunately missing as of that version.
    
    Introduced a value object to encapsulate the mitigation behavior and make client code unaware of that.

---
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/GeoCommandStatisticsUnitTests.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.data.mongodb.core;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.mongodb.BasicDBObject;
+
+/**
+ * Unit tests for {@link GeoCommandStatistics}.
+ * 
+ * @author Oliver Gierke
+ * @soundtrack Fruitcake - Jeff Coffin (The Inside of the Outside)
+ */
+public class GeoCommandStatisticsUnitTests {
+
+	/**
+	 * @see DATAMONGO-1361
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void rejectsNullCommandResult() {
+		GeoCommandStatistics.from(null);
+	}
+
+	/**
+	 * @see DATAMONGO-1361
+	 */
+	@Test
+	public void fallsBackToNanIfNoAverageDistanceIsAvailable() {
+
+		GeoCommandStatistics statistics = GeoCommandStatistics.from(new BasicDBObject("stats", null));
+		assertThat(statistics.getAverageDistance(), is(Double.NaN));
+
+		statistics = GeoCommandStatistics.from(new BasicDBObject("stats", new BasicDBObject()));
+		assertThat(statistics.getAverageDistance(), is(Double.NaN));
+	}
+
+	/**
+	 * @see DATAMONGO-1361
+	 */
+	@Test
+	public void returnsAverageDistanceIfPresent() {
+
+		GeoCommandStatistics statistics = GeoCommandStatistics
+				.from(new BasicDBObject("stats", new BasicDBObject("avgDistance", 1.5)));
+
+		assertThat(statistics.getAverageDistance(), is(1.5));
+	}
+}
