9b802b931052bc2e161c40a8d8c0a4cf3d2a9d5f
Christoph Strobl <cstrobl@pivotal.io>
2016-04-28 10:46:44 +0200

    DATAMONGO-1423 - Fix custom enum map.key conversion for Update.
    We now pipe map keys through the Converter when mapping Updates.

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/convert/UpdateMapperUnitTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/convert/UpdateMapperUnitTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 the original author or authors.
+ * Copyright 2013-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -887,6 +887,32 @@ public class UpdateMapperUnitTests {
 		assertThat($set.get("primIntValue"), Is.<Object> is(10));
 	}
 
+	/**
+	 * @see DATAMONGO-1423
+	 */
+	@Test
+	public void mappingShouldConsiderCustomConvertersForEnumMapKeys() {
+
+		CustomConversions conversions = new CustomConversions(
+				Arrays.asList(AllocationToStringConverter.INSTANCE, StringToAllocationConverter.INSTANCE));
+
+		MongoMappingContext mappingContext = new MongoMappingContext();
+		mappingContext.setSimpleTypeHolder(conversions.getSimpleTypeHolder());
+		mappingContext.afterPropertiesSet();
+
+		MappingMongoConverter converter = new MappingMongoConverter(mock(DbRefResolver.class), mappingContext);
+		converter.setCustomConversions(conversions);
+		converter.afterPropertiesSet();
+
+		UpdateMapper mapper = new UpdateMapper(converter);
+
+		Update update = new Update().set("enumAsMapKey", Collections.singletonMap(Allocation.AVAILABLE, 100));
+		DBObject result = mapper.getMappedObject(update.getUpdateObject(),
+				mappingContext.getPersistentEntity(ClassWithEnum.class));
+
+		assertThat(result, isBsonObject().containing("$set.enumAsMapKey.V", 100));
+	}
+
 	static class DomainTypeWrappingConcreteyTypeHavingListOfInterfaceTypeAttributes {
 		ListModelWrapper concreteTypeWithListAttributeOfInterfaceType;
 	}
@@ -1113,6 +1139,7 @@ public class UpdateMapperUnitTests {
 	static class ClassWithEnum {
 
 		Allocation allocation;
+		Map<Allocation, String> enumAsMapKey;
 
 		static enum Allocation {
 
