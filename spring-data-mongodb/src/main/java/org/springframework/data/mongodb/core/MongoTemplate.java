512f68a611ea22df34efa6f5ac2542edc11fdd17
Mark Paluch <mpaluch@pivotal.io>
2016-08-23 10:01:46 +0200

    DATAMONGO-1476 - Polishing.
    Extend year range in license header. Use the specified collection name in doRemove.
    
    Original pull request: #382.

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/MongoTemplate.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/MongoTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2015 the original author or authors.
+ * Copyright 2010-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1336,7 +1336,7 @@ public class MongoTemplate implements MongoOperations, ApplicationContextAware {
 
 				if (LOGGER.isDebugEnabled()) {
 					LOGGER.debug("Remove using query: {} in collection: {}.",
-							new Object[] { serializeToJsonSafely(dboq), collection.getName() });
+							new Object[] { serializeToJsonSafely(dboq), collectionName });
 				}
 
 				WriteResult wr = writeConcernToUse == null ? collection.remove(dboq)
