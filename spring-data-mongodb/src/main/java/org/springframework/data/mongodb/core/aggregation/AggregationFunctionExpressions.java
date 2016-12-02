4dd27dbaa4c9755691197292681c36124b4e5ed0
Christoph Strobl <cstrobl@pivotal.io>
2016-12-02 15:54:30 +0100

    DATAMONGO-1540 - Add support for $map (aggregation).
    We now support $map operator in aggregation.

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/aggregation/AggregationFunctionExpressions.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/aggregation/AggregationFunctionExpressions.java
@@ -36,7 +36,7 @@ import com.mongodb.DBObject;
 @Deprecated
 public enum AggregationFunctionExpressions {
 
-	SIZE, CMP, EQ, GT, GTE, LT, LTE, NE, SUBTRACT;
+	SIZE, CMP, EQ, GT, GTE, LT, LTE, NE, SUBTRACT, ADD;
 
 	/**
 	 * Returns an {@link AggregationExpression} build from the current {@link Enum} name and the given parameters.
