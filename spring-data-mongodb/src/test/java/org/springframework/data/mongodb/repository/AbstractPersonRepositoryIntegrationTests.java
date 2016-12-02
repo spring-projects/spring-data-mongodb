40da4701de5426270aa0a03cdfb7b164fe0d616c
Oliver Gierke <ogierke@pivotal.io>
2016-12-02 16:33:13 +0100

    DATAMONGO-1454 - Polishing.
    Formatting in test case.
    
    Original pull request: #381.

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/repository/AbstractPersonRepositoryIntegrationTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/repository/AbstractPersonRepositoryIntegrationTests.java
@@ -594,6 +594,7 @@ public abstract class AbstractPersonRepositoryIntegrationTests {
 	 */
 	@Test
 	public void executesDerivedExistsProjectionToBoolean() {
+
 		assertThat(repository.existsByFirstname("Oliver August"), is(true));
 		assertThat(repository.existsByFirstname("Hans Peter"), is(false));
 	}
