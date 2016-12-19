29e405b800e10f470362a4cc0025b2ab9a971ab6
Oliver Gierke <ogierke@pivotal.io>
2016-12-19 14:26:12 +0100

    DATAMONGO-1565 - Polishing.
    Formatting in ExpressionEvaluatingParameterBinder and StringBasedMongoQueryUnitTests. Turned Placeholder into value object.

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/repository/query/StringBasedMongoQueryUnitTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/repository/query/StringBasedMongoQueryUnitTests.java
@@ -366,6 +366,17 @@ public class StringBasedMongoQueryUnitTests {
 	}
 
 	/**
+	 * @see DATAMONGO-1454
+	 */
+	@Test
+	public void shouldSupportExistsProjection() throws Exception {
+
+		StringBasedMongoQuery mongoQuery = createQueryForMethod("existsByLastname", String.class);
+
+		assertThat(mongoQuery.isExistsQuery(), is(true));
+	}
+
+	/**
 	 * @see DATAMONGO-1565
 	 */
 	@Test
@@ -383,17 +394,6 @@ public class StringBasedMongoQueryUnitTests {
 		org.springframework.data.mongodb.core.query.Query reference = new BasicQuery(queryObject);
 
 		assertThat(query.getQueryObject(), is(reference.getQueryObject()));
-	}
-
-	/**
-	 * @see DATAMONGO-1454
-	 */
-	@Test
-	public void shouldSupportExistsProjection() throws Exception {
-
-		StringBasedMongoQuery mongoQuery = createQueryForMethod("existsByLastname", String.class);
-
-		assertThat(mongoQuery.isExistsQuery(), is(true));
 	}
 
 	/**
