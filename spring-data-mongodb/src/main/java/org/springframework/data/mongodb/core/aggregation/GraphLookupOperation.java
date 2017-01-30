eb97944fd4b10226064f687ff82d559d957bda38
Mark Paluch <mpaluch@pivotal.io>
2017-01-30 10:30:18 +0100

    DATAMONGO-1600 - Make GraphLookupOperationBuilder public.
    Make GraphLookupOperationBuilder public so it can be used in types outside the aggregation package.
    
    Original Pull Request: #437

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/aggregation/GraphLookupOperation.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/aggregation/GraphLookupOperation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -330,7 +330,7 @@ public class GraphLookupOperation implements InheritsFieldsAggregationOperation 
 	/**
 	 * @author Mark Paluch
 	 */
-	static final class GraphLookupOperationBuilder {
+	public static final class GraphLookupOperationBuilder {
 
 		private final String from;
 		private final List<Object> startWith;
