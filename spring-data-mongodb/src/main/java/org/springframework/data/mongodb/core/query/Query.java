9c035a190a76b99ecd6246c5e5320b9cef467c2e
Christoph Strobl <cstrobl@pivotal.io>
2017-03-06 09:03:34 +0100

    DATAMONGO-1421 - Fix serialization in error message causing error itself.
    We now make sure to safely serialize the criteria object used for creating the error message when raising an `InvalidMongoDbApiUsageException` in cases where `addCriteria` is used to add multiple entries for the same property.
    
    Original pull request: #448.

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/query/Query.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/query/Query.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2016 the original author or authors.
+ * Copyright 2010-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -97,7 +97,7 @@ public class Query {
 		} else {
 			throw new InvalidMongoDbApiUsageException(
 					"Due to limitations of the com.mongodb.BasicDBObject, " + "you can't add a second '" + key + "' criteria. "
-							+ "Query already contains '" + existing.getCriteriaObject() + "'.");
+							+ "Query already contains '" + serializeToJsonSafely(existing.getCriteriaObject()) + "'.");
 		}
 
 		return this;
