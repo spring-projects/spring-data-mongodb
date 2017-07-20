7258cb8d1d35f6404c08a464f5bb762c4c490231
Oliver Gierke <ogierke@pivotal.io>
2017-07-20 13:08:16 +0200

    DATAMONGO-1748 - Polishing.
    Added missing license headers, formatting.
    
    Original pull request: #473.

--- spring-data-mongodb/src/test/kotlin/org/springframework/data/mongodb/core/query/CriteriaExtensionsTests.kt
+++ spring-data-mongodb/src/test/kotlin/org/springframework/data/mongodb/core/query/CriteriaExtensionsTests.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.mongodb.core.query
 
 import org.junit.Test
@@ -18,17 +33,22 @@ class CriteriaExtensionsTests {
 
 	@Test
 	fun `isEqualTo() extension should call its Java counterpart`() {
+
 		val foo = "foo"
+
 		criteria.isEqualTo(foo)
+
 		Mockito.verify(criteria, Mockito.times(1)).`is`(foo)
 	}
 
 	@Test
 	fun `inValues() extension should call its Java counterpart`() {
+
 		val foo = "foo"
 		val bar = "bar"
+
 		criteria.inValues(foo, bar)
+
 		Mockito.verify(criteria, Mockito.times(1)).`in`(foo, bar)
 	}
-
 }
