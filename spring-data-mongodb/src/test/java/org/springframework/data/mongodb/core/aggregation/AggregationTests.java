3f1704719da4935db26b2d488240baf8ee055487
Minsu <iqjumps@gmail.com>
2019-03-03 03:09:55 +0900

    DATAMONGO-2217 - Fix error in test code

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/aggregation/AggregationTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/aggregation/AggregationTests.java
@@ -1416,10 +1416,11 @@ public class AggregationTests {
 		mongoTemplate.dropCollection(ObjectWithDate.class);
 
 		DateTime dateTime = new DateTime() //
+				.withZone(DateTimeZone.UTC) //
 				.withYear(2014) //
 				.withMonthOfYear(2) //
 				.withDayOfMonth(7) //
-				.withTime(3, 4, 5, 6).toDateTime(DateTimeZone.UTC).toDateTimeISO();
+				.withTime(3, 4, 5, 6).toDateTimeISO();
 
 		ObjectWithDate owd = new ObjectWithDate(dateTime.toDate());
 		mongoTemplate.insert(owd);
