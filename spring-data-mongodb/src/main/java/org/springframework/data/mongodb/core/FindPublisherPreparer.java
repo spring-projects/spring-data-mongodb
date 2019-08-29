f3a7d6a20ed1612177ffdfaa00899ab16e50fcd4
kostya05983 <kostya05983@mail.ru>
2019-08-30 01:10:18 +0700

    DATAMONGO-2354 - Change visibility of FindPublisherPreparer.
    The FindPublisherPreparer is used in an protected method of ReactiveMongoTemplate and needs to be public to allow overriding.
    
    Original Pull Request: #784

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/FindPublisherPreparer.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/FindPublisherPreparer.java
@@ -21,8 +21,9 @@ import com.mongodb.reactivestreams.client.FindPublisher;
  * Simple callback interface to allow customization of a {@link FindPublisher}.
  *
  * @author Mark Paluch
+ * @author Konstantin Volivach
  */
-interface FindPublisherPreparer {
+public interface FindPublisherPreparer {
 
 	/**
 	 * Prepare the given cursor (apply limits, skips and so on). Returns the prepared cursor.
