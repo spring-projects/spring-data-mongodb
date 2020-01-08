b4222c5646c602ec055e6a550ab8d7f2e962d860
Christoph Strobl <cstrobl@pivotal.io>
2020-01-08 08:43:10 +0100

    DATAMONGO-2442 - Polishing.
    Transition touched test classes to JUnit Jupiter.
    
    Original pull request: #818.

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/aggregation/CondExpressionUnitTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/aggregation/CondExpressionUnitTests.java
@@ -21,7 +21,7 @@ import static org.springframework.data.mongodb.test.util.Assertions.*;
 import java.util.Arrays;
 
 import org.bson.Document;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.data.mongodb.core.aggregation.ConditionalOperators.Cond;
 import org.springframework.data.mongodb.core.query.Criteria;
 
