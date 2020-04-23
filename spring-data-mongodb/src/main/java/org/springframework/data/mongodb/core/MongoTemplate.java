cecd47d679f7d15c0d9441ca7c98047abc9954f0
Mark Paluch <mpaluch@pivotal.io>
2020-04-23 14:57:52 +0200

    DATAMONGO-2529 - Ensure that MappingMongoConverter.read(…) is never called with null.
    Previously, various methods attempted to pass a null argument as source for the converter. The API is non-null and implementations relying on these constraints were easily breakable.
    
    We now make sure that the source is never null.

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/MongoTemplate.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/MongoTemplate.java
@@ -3155,11 +3155,12 @@ public class MongoTemplate implements MongoOperations, ApplicationContextAware, 
 		@Nullable
 		public T doWith(@Nullable Document object) {
 
+			T source = null;
+
 			if (null != object) {
 				maybeEmitEvent(new AfterLoadEvent<>(object, type, collectionName));
+				source = reader.read(type, object);
 			}
-
-			T source = reader.read(type, object);
 
 			if (null != source) {
 				maybeEmitEvent(new AfterConvertEvent<>(object, source, collectionName));
@@ -3200,9 +3201,7 @@ public class MongoTemplate implements MongoOperations, ApplicationContextAware, 
 			Class<?> typeToRead = targetType.isInterface() || targetType.isAssignableFrom(entityType) ? entityType
 					: targetType;
 
-			if (null != object) {
-				maybeEmitEvent(new AfterLoadEvent<>(object, targetType, collectionName));
-			}
+			maybeEmitEvent(new AfterLoadEvent<>(object, targetType, collectionName));
 
 			Object source = reader.read(typeToRead, object);
 			Object result = targetType.isInterface() ? projectionFactory.createProjection(targetType, source) : source;
