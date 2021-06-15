e2d901ff299c88c6a96449d0b9f3cb52091cf72e
Piotr Kubowicz <piotr.kubowicz@gmail.com>
2021-06-15 09:57:08 +0200

    DATAMONGO-2511 Give context to mapping exceptions
    When invoking findAll() exception details contained no information
    which document was malformed and other details (for example which
    field was wrong) were usually useless for identifying the document.
    
    Now the mapping exception will include the document.

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/convert/MappingMongoConverterUnitTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/convert/MappingMongoConverterUnitTests.java
@@ -856,8 +856,9 @@ class MappingMongoConverterUnitTests {
 
 		org.bson.Document document = new org.bson.Document("foo", "bar");
 
-		assertThatThrownBy(() -> converter.read(DefaultedConstructorArgument.class, document))
-				.isInstanceOf(MappingInstantiationException.class);
+		assertThatThrownBy(() -> converter.read(DefaultedConstructorArgument.class, document)) //
+				.isInstanceOf(MappingException.class) //
+				.hasCauseInstanceOf(MappingInstantiationException.class);
 	}
 
 	@Test // DATAMONGO-358
@@ -1855,8 +1856,9 @@ class MappingMongoConverterUnitTests {
 
 		org.bson.Document document = new org.bson.Document("property", InterfacedEnum.INSTANCE.name());
 
-		assertThatExceptionOfType(ConverterNotFoundException.class) //
-				.isThrownBy(() -> converter.read(DocWithInterfacedEnum.class, document));
+		assertThatThrownBy(() -> converter.read(DocWithInterfacedEnum.class, document)) //
+				.isInstanceOf(MappingException.class) //
+				.hasCauseInstanceOf(ConverterNotFoundException.class);
 	}
 
 	@Test // DATAMONGO-1898
