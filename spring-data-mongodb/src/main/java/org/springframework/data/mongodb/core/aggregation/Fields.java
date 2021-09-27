0917e687baa4ac94fa1cdb66b3aaa1a8ac57d25d
Divya Srivastava <divya.jnu08@gmail.com>
2021-09-28 05:00:09 +0530

    DATAMONGO-2637 - Fields and projection API are very inconvenient

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/aggregation/Fields.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/aggregation/Fields.java
@@ -233,14 +233,9 @@ public final class Fields implements Iterable<Field> {
 			String targetToSet = target != null ? cleanUp(target) : null;
 
 			Assert.hasText(nameToSet, "AggregationField name must not be null or empty!");
-
-			if (target == null && name.contains(".")) {
-				this.name = nameToSet.substring(nameToSet.indexOf('.') + 1);
-				this.target = nameToSet;
-			} else {
-				this.name = nameToSet;
-				this.target = targetToSet;
-			}
+			
+			this.name = nameToSet;
+			this.target = target == null ? nameToSet : targetToSet; 
 		}
 
 		private static String cleanUp(String source) {
