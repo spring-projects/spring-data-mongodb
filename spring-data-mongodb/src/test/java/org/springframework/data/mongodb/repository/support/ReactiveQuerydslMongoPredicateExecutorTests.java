9c78802c47a9c1d4782935b5f31ec069087f88fd
Mark Paluch <mpaluch@vmware.com>
2022-01-12 15:30:23 +0100

    Polishing.
    Add author tags, extend copyright license years, simplify tests.
    
    See #3892

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/repository/support/ReactiveQuerydslMongoPredicateExecutorTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/repository/support/ReactiveQuerydslMongoPredicateExecutorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -65,6 +65,7 @@ import com.mongodb.reactivestreams.client.MongoDatabase;
  *
  * @author Mark Paluch
  * @author Christoph Strobl
+ * @author Rocco Lagrotteria
  */
 @RunWith(SpringRunner.class)
 @ContextConfiguration
@@ -400,9 +401,8 @@ public class ReactiveQuerydslMongoPredicateExecutorTests {
 				.as(StepVerifier::create) //
 				.assertNext(it -> {
 
-					assertThat(it.getContent().size()).isEqualTo(1);
 					assertThat(it.getTotalElements()).isEqualTo(2);
-					assertThat(it.getContent()).contains(dave);
+					assertThat(it.getContent()).containsOnly(dave);
 				}).verifyComplete();
 
 		repository
@@ -410,9 +410,8 @@ public class ReactiveQuerydslMongoPredicateExecutorTests {
 				.as(StepVerifier::create) //
 				.assertNext(it -> {
 
-					assertThat(it.getContent().size()).isEqualTo(1);
 					assertThat(it.getTotalElements()).isEqualTo(2);
-					assertThat(it.getContent()).contains(oliver);
+					assertThat(it.getContent()).containsOnly(oliver);
 				}).verifyComplete();
 	}
 
