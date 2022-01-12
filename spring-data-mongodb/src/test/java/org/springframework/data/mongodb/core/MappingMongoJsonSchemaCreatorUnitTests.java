04ec49eb9ef8fe006756619dea165f2fc512ed3e
Christoph Strobl <cstrobl@vmware.com>
2022-01-12 09:29:43 +0100

    Avoid schema keyId uuid representation errors.
    To avoid driver configuration specific UUID representation format errors (binary subtype 3 vs. subtype 4) we now directly convert the given key into its subtype 4 format.
    
    Resolves: #3929
    Original pull request: #3931.

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/MappingMongoJsonSchemaCreatorUnitTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/MappingMongoJsonSchemaCreatorUnitTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.bson.BsonDocument;
 import org.bson.Document;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -107,7 +108,7 @@ public class MappingMongoJsonSchemaCreatorUnitTests {
 				.createSchemaFor(Patient.class);
 
 		Document targetSchema = schema.schemaDocument();
-		assertThat(targetSchema).isEqualTo(Document.parse(PATIENT));
+		assertThat(targetSchema.toBsonDocument()).isEqualTo(BsonDocument.parse(PATIENT));
 	}
 
 	@Test // GH-3800
@@ -136,7 +137,7 @@ public class MappingMongoJsonSchemaCreatorUnitTests {
 				.filter(MongoJsonSchemaCreator.encryptedOnly()) //
 				.createSchemaFor(EncryptionMetadataFromProperty.class);
 
-		assertThat(schema.schemaDocument()).isEqualTo(Document.parse(ENC_FROM_PROPERTY_SCHEMA));
+		assertThat(schema.schemaDocument().toBsonDocument()).isEqualTo(BsonDocument.parse(ENC_FROM_PROPERTY_SCHEMA));
 	}
 
 	@Test // GH-3800
@@ -154,7 +155,7 @@ public class MappingMongoJsonSchemaCreatorUnitTests {
 				.filter(MongoJsonSchemaCreator.encryptedOnly()) //
 				.createSchemaFor(EncryptionMetadataFromMethod.class);
 
-		assertThat(schema.schemaDocument()).isEqualTo(Document.parse(ENC_FROM_METHOD_SCHEMA));
+		assertThat(schema.schemaDocument().toBsonDocument()).isEqualTo(BsonDocument.parse(ENC_FROM_METHOD_SCHEMA));
 	}
 
 	// --> TYPES AND JSON
@@ -392,7 +393,7 @@ public class MappingMongoJsonSchemaCreatorUnitTests {
 	}
 
 	static final String ENC_FROM_PROPERTY_ENTITY_KEY = "C5a5aMB7Ttq4wSJTFeRn8g==";
-	static final String ENC_FROM_PROPERTY_PROPOERTY_KEY = "Mw6mdTVPQfm4quqSCLVB3g=";
+	static final String ENC_FROM_PROPERTY_PROPOERTY_KEY = "Mw6mdTVPQfm4quqSCLVB3g==";
 	static final String ENC_FROM_PROPERTY_SCHEMA = "{" + //
 			"  'encryptMetadata': {" + //
 			"    'keyId': [" + //
