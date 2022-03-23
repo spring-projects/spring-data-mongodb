c272c7317e2b62cc78d3c7c2da2025a053449281
Christoph Strobl <cstrobl@vmware.com>
2022-03-23 12:03:25 +0100

    Fix rewrite near & nearSphere count queries using geoJson to geoWithin.
    $near and $nearSphere queries are not supported via countDocuments and the used aggregation match stage and need to be rewritten to $geoWithin. The existing logic did not cover usage of geoJson types, which is fixed now. In case of nearSphere it is also required to convert the $maxDistance argument (given in meters for geoJson) to radians which is used by $geoWithin $centerSphere.
    
    Closes #4004
    Original pull request: #4006.
    Related to #2925

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/query/MetricConversion.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/query/MetricConversion.java
@@ -17,6 +17,7 @@
 package org.springframework.data.mongodb.core.query;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.math.RoundingMode;
 
 import org.springframework.data.geo.Distance;
@@ -27,6 +28,7 @@ import org.springframework.data.geo.Metrics;
  * {@link Metric} and {@link Distance} conversions using the metric system.
  *
  * @author Mark Paluch
+ * @author Christoph Strobl
  * @since 2.2
  */
 public class MetricConversion {
@@ -59,6 +61,28 @@ public class MetricConversion {
 	public static double getDistanceInMeters(Distance distance) {
 		return new BigDecimal(distance.getValue()).multiply(getMetricToMetersMultiplier(distance.getMetric()))
 				.doubleValue();
+	}
+
+	/**
+	 * Return {@code distance} in radians (on an earth like sphere).
+	 *
+	 * @param distance must not be {@literal null}.
+	 * @return distance in rads.
+	 * @since 3.4
+	 */
+	public static double toRadians(Distance distance) {
+		return metersToRadians(getDistanceInMeters(distance));
+	}
+
+	/**
+	 * Return {@code distance} in radians (on an earth like sphere).
+	 *
+	 * @param meters
+	 * @return distance in rads.
+	 * @since 3.4
+	 */
+	public static double metersToRadians(double meters) {
+		return BigDecimal.valueOf(meters).divide(METERS_MULTIPLIER, MathContext.DECIMAL64).doubleValue();
 	}
 
 	/**
