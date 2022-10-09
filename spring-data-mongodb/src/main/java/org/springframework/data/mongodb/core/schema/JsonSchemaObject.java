c9ad0884ec8863d943ae628ef31c303e55657150
gongxuanzhang <GxZ460421123>
2022-10-09 10:30:14 +0800

    Fix json schema type name for boolean.
    Was boolean should have been bool.
    
    Closes: #4196

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/schema/JsonSchemaObject.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/schema/JsonSchemaObject.java
@@ -295,7 +295,7 @@ public interface JsonSchemaObject {
 		Type OBJECT = jsonTypeOf("object");
 		Type ARRAY = jsonTypeOf("array");
 		Type NUMBER = jsonTypeOf("number");
-		Type BOOLEAN = jsonTypeOf("boolean");
+		Type BOOLEAN = jsonTypeOf("bool");
 		Type STRING = jsonTypeOf("string");
 		Type NULL = jsonTypeOf("null");
 
