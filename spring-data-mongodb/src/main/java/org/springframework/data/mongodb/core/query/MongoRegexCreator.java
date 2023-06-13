c48b2c6b9bf6d010e2c4e1453c87925ac06a2bc5
Christoph Strobl <cstrobl@vmware.com>
2023-06-13 08:26:43 +0200

    Use exact matching for IN clause with ignore case.
    Prior to this change the generated pattern would have matched more entries than it should have. The behavior is now aligned to its counterpart not using the IgnoreCase flag.
    
    Closes #4404
    Original pull request: #4412

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/query/MongoRegexCreator.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/query/MongoRegexCreator.java
@@ -107,7 +107,9 @@ public enum MongoRegexCreator {
 	 * @param source
 	 * @return
 	 * @since 2.2.14
+	 * @deprecated since 4.0.7
 	 */
+	@Deprecated(since = "4.0.7", forRemoval = true)
 	public Object toCaseInsensitiveMatch(Object source) {
 		return source instanceof String ? new BsonRegularExpression(Pattern.quote((String) source), "i") : source;
 	}
