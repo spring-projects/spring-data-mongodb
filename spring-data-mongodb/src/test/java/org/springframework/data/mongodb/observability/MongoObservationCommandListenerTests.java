69a5022ba55396127a301c56794b9b1b6444bc71
Mark Paluch <mark.paluch@broadcom.com>
2024-01-23 14:27:28 +0100

    Reinstate observability tests.

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/observability/MongoObservationCommandListenerTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/observability/MongoObservationCommandListenerTests.java
@@ -30,6 +30,7 @@ import org.bson.BsonDocument;
 import org.bson.BsonString;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.data.mongodb.observability.MongoObservation.LowCardinalityCommandKeyNames;
 
 import com.mongodb.RequestContext;
@@ -100,115 +101,115 @@ class MongoObservationCommandListenerTests {
 	void successfullyCompletedCommandShouldCreateTimerWhenParentSampleInRequestContext() {
 
 		// given
-//		Observation parent = Observation.start("name", observationRegistry);
-//		RequestContext traceRequestContext = getContext();
-//
-//		// when
-//		listener.commandStarted(new CommandStartedEvent(traceRequestContext, 0, //
-//				new ConnectionDescription( //
-//						new ServerId( //
-//								new ClusterId("description"), //
-//								new ServerAddress("localhost", 1234))),
-//				"database", "insert", //
-//				new BsonDocument("collection", new BsonString("user"))));
-//		listener.commandSucceeded(new CommandSucceededEvent(traceRequestContext, 0, null, "insert", null, 0));
-//
-//		// then
-//		assertThatTimerRegisteredWithTags();
+		Observation parent = Observation.start("name", observationRegistry);
+		RequestContext traceRequestContext = getContext();
+
+		// when
+		listener.commandStarted(new CommandStartedEvent(traceRequestContext, 0, 0, //
+				new ConnectionDescription( //
+						new ServerId( //
+								new ClusterId("description"), //
+								new ServerAddress("localhost", 1234))),
+				"database", "insert", //
+				new BsonDocument("collection", new BsonString("user"))));
+		listener.commandSucceeded(new CommandSucceededEvent(traceRequestContext, 0, 0, null, "insert", null, null, 0));
+
+		// then
+		assertThatTimerRegisteredWithTags();
 	}
 
 	@Test
 	void successfullyCompletedCommandWithCollectionHavingCommandNameShouldCreateTimerWhenParentSampleInRequestContext() {
 
 		// given
-//		Observation parent = Observation.start("name", observationRegistry);
-//		RequestContext traceRequestContext = getContext();
-//
-//		// when
-//		listener.commandStarted(new CommandStartedEvent(traceRequestContext, 0, //
-//				new ConnectionDescription( //
-//						new ServerId( //
-//								new ClusterId("description"), //
-//								new ServerAddress("localhost", 1234))), //
-//				"database", "aggregate", //
-//				new BsonDocument("aggregate", new BsonString("user"))));
-//		listener.commandSucceeded(new CommandSucceededEvent(traceRequestContext, 0, null, "aggregate", null, 0));
+		Observation parent = Observation.start("name", observationRegistry);
+		RequestContext traceRequestContext = getContext();
+
+		// when
+		listener.commandStarted(new CommandStartedEvent(traceRequestContext, 0, 0, //
+				new ConnectionDescription( //
+						new ServerId( //
+								new ClusterId("description"), //
+								new ServerAddress("localhost", 1234))), //
+				"database", "aggregate", //
+				new BsonDocument("aggregate", new BsonString("user"))));
+		listener.commandSucceeded(new CommandSucceededEvent(traceRequestContext, 0, 0, null, "aggregate", null, null, 0));
 
 		// then
-//		assertThatTimerRegisteredWithTags();
+		assertThatTimerRegisteredWithTags();
 	}
 
 	@Test
 	void successfullyCompletedCommandWithoutClusterInformationShouldCreateTimerWhenParentSampleInRequestContext() {
 
-//		// given
-//		Observation parent = Observation.start("name", observationRegistry);
-//		RequestContext traceRequestContext = getContext();
-//
-//		// when
-//		listener.commandStarted(new CommandStartedEvent(traceRequestContext, 0, null, "database", "insert",
-//				new BsonDocument("collection", new BsonString("user"))));
-//		listener.commandSucceeded(new CommandSucceededEvent(traceRequestContext, 0, null, "insert", null, 0));
-//
-//		assertThat(meterRegistry).hasTimerWithNameAndTags(MongoObservation.MONGODB_COMMAND_OBSERVATION.getName(),
-//				KeyValues.of(LowCardinalityCommandKeyNames.MONGODB_COLLECTION.withValue("user"),
-//						LowCardinalityCommandKeyNames.DB_NAME.withValue("database"),
-//						LowCardinalityCommandKeyNames.MONGODB_COMMAND.withValue("insert"),
-//						LowCardinalityCommandKeyNames.DB_SYSTEM.withValue("mongodb")).and("error", "none"));
+		// given
+		Observation parent = Observation.start("name", observationRegistry);
+		RequestContext traceRequestContext = getContext();
+
+		// when
+		listener.commandStarted(new CommandStartedEvent(traceRequestContext, 0, 0, null, "database", "insert",
+				new BsonDocument("collection", new BsonString("user"))));
+		listener.commandSucceeded(new CommandSucceededEvent(traceRequestContext, 0, 0, null, "insert", null, null, 0));
+
+		assertThat(meterRegistry).hasTimerWithNameAndTags(MongoObservation.MONGODB_COMMAND_OBSERVATION.getName(),
+				KeyValues.of(LowCardinalityCommandKeyNames.MONGODB_COLLECTION.withValue("user"),
+						LowCardinalityCommandKeyNames.DB_NAME.withValue("database"),
+						LowCardinalityCommandKeyNames.MONGODB_COMMAND.withValue("insert"),
+						LowCardinalityCommandKeyNames.DB_SYSTEM.withValue("mongodb")).and("error", "none"));
 	}
 
 	@Test
 	void commandWithErrorShouldCreateTimerWhenParentSampleInRequestContext() {
 
-//		// given
-//		Observation parent = Observation.start("name", observationRegistry);
-//		RequestContext traceRequestContext = getContext();
-//
-//		// when
-//		listener.commandStarted(new CommandStartedEvent(traceRequestContext, 0, //
-//				new ConnectionDescription( //
-//						new ServerId( //
-//								new ClusterId("description"), //
-//								new ServerAddress("localhost", 1234))), //
-//				"database", "insert", //
-//				new BsonDocument("collection", new BsonString("user"))));
-//		listener.commandFailed( //
-//				new CommandFailedEvent(traceRequestContext, 0, null, "insert", 0, new IllegalAccessException()));
-//
-//		// then
-//		assertThatTimerRegisteredWithTags();
+		// given
+		Observation parent = Observation.start("name", observationRegistry);
+		RequestContext traceRequestContext = getContext();
+
+		// when
+		listener.commandStarted(new CommandStartedEvent(traceRequestContext, 0, 0, //
+				new ConnectionDescription( //
+						new ServerId( //
+								new ClusterId("description"), //
+								new ServerAddress("localhost", 1234))), //
+				"database", "insert", //
+				new BsonDocument("collection", new BsonString("user"))));
+		listener.commandFailed( //
+				new CommandFailedEvent(traceRequestContext, 0, 0, null, "db", "insert", 0, new IllegalAccessException()));
+
+		// then
+		assertThatTimerRegisteredWithTags();
 	}
 
 	@Test // GH-4481
 	void completionShouldIgnoreIncompatibleObservationContext() {
 
-//		// given
-//		RequestContext traceRequestContext = getContext();
-//
-//		Observation observation = mock(Observation.class);
-//		traceRequestContext.put(ObservationThreadLocalAccessor.KEY, observation);
-//
-//		// when
-//		listener.commandSucceeded(new CommandSucceededEvent(traceRequestContext, 0, null, "insert", null, 0));
-//
-//		verify(observation).getContext();
-//		verifyNoMoreInteractions(observation);
+		// given
+		RequestContext traceRequestContext = getContext();
+
+		Observation observation = mock(Observation.class);
+		traceRequestContext.put(ObservationThreadLocalAccessor.KEY, observation);
+
+		// when
+		listener.commandSucceeded(new CommandSucceededEvent(traceRequestContext, 0, 0, null, "insert", null, null, 0));
+
+		verify(observation).getContext();
+		verifyNoMoreInteractions(observation);
 	}
 
 	@Test // GH-4481
 	void failureShouldIgnoreIncompatibleObservationContext() {
 
-//		// given
-//		RequestContext traceRequestContext = getContext();
-//
-//		Observation observation = mock(Observation.class);
-//		traceRequestContext.put(ObservationThreadLocalAccessor.KEY, observation);
-//
-//		// when
-//		listener.commandFailed(new CommandFailedEvent(traceRequestContext, 0, null, "insert", 0, null));
-//
-//		verify(observation).getContext();
-//		verifyNoMoreInteractions(observation);
+		// given
+		RequestContext traceRequestContext = getContext();
+
+		Observation observation = mock(Observation.class);
+		traceRequestContext.put(ObservationThreadLocalAccessor.KEY, observation);
+
+		// when
+		listener.commandFailed(new CommandFailedEvent(traceRequestContext, 0, 0, null, "db", "insert", 0, null));
+
+		verify(observation).getContext();
+		verifyNoMoreInteractions(observation);
 	}
 
 	private RequestContext getContext() {
