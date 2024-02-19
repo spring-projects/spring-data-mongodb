954f52ca7993cc4806b6e82adc50f8d5e3ae9fb9
Mark Paluch <mark.paluch@broadcom.com>
2024-02-19 15:25:25 +0100

    Apply `AggregationOptions.maxTime` to `MongoTemplate.aggregateStream()`.
    Closes #4644

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/MongoTemplate.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/MongoTemplate.java
@@ -2268,6 +2268,10 @@ public class MongoTemplate
 				cursor = hintFunction.apply(mongoDbFactory, cursor::hintString, cursor::hint);
 			}
 
+			if (options.hasExecutionTimeLimit()) {
+				cursor = cursor.maxTime(options.getMaxTime().toMillis(), TimeUnit.MILLISECONDS);
+			}
+
 			Class<?> domainType = aggregation instanceof TypedAggregation typedAggregation ? typedAggregation.getInputType()
 					: null;
 
