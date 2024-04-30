cdd4a013918058340f58afc48ddec39625ef2917
Christoph Strobl <christoph.strobl@broadcom.com>
2024-04-30 09:59:01 +0200

    Fix NPE when rendering untyped aggregation operation as part of criteria query.
    Resolves: #4687
    Original pull request: #4695

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/convert/QueryMapperUnitTests.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/convert/QueryMapperUnitTests.java
@@ -1604,6 +1604,15 @@ public class QueryMapperUnitTests {
 		assertThat(mappedObject).isEqualTo("{ $expr : { $expr : { $gt : [ '$foo', '$budget'] } } }");
 	}
 
+	@Test // GH-4687
+	void usageOfUntypedAggregationShouldRenderOperationsAsIs() {
+
+		Query query = query(expr(Expr.valueOf(ComparisonOperators.valueOf("field").greaterThan("budget"))));
+		org.bson.Document mappedObject = mapper.getMappedObject(query.getQueryObject(),
+			context.getPersistentEntity(Object.class));
+		assertThat(mappedObject).isEqualTo("{ $expr : { $expr : { $gt : [ '$field', '$budget'] } } }");
+	}
+
 	@Test // GH-2750
 	void usesMongoExpressionDocumentAsIsIfItIsNotAnAggregationExpression() {
 
