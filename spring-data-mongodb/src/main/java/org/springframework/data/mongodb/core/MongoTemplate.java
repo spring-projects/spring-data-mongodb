4ebcdf590bbba45ee32ca149b2100cf048785cbd
Mark Paluch <mark.paluch@broadcom.com>
2025-02-13 15:35:16 +0100

    Polishing.
    Reformat code. Reuse PersistentEntity for mapping instead of resolving TypeInformation from Class.
    
    Original pull request: #4892
    See: #4860

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/MongoTemplate.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/MongoTemplate.java
@@ -2624,7 +2624,9 @@ public class MongoTemplate
 
 		if (LOGGER.isDebugEnabled()) {
 
-			Document mappedSort = preparer instanceof SortingQueryCursorPreparer sqcp ?  getMappedSortObject(sqcp.getSortObject(), sourceClass) : null;
+			Document mappedSort = preparer instanceof SortingQueryCursorPreparer sqcp
+					? getMappedSortObject(sqcp.getSortObject(), entity)
+					: null;
 			LOGGER.debug(String.format("find using query: %s fields: %s sort: %s for class: %s in collection: %s",
 					serializeToJsonSafely(mappedQuery), mappedFields, serializeToJsonSafely(mappedSort), sourceClass,
 					collectionName));
@@ -2993,7 +2995,7 @@ public class MongoTemplate
 	}
 
 	@Nullable
-	private Document getMappedSortObject(Document sortObject, MongoPersistentEntity<?> entity) {
+	private Document getMappedSortObject(Document sortObject, @Nullable MongoPersistentEntity<?> entity) {
 
 		if (ObjectUtils.isEmpty(sortObject)) {
 			return null;
@@ -3355,11 +3357,8 @@ public class MongoTemplate
 	class QueryCursorPreparer implements SortingQueryCursorPreparer {
 
 		private final Query query;
-
 		private final Document sortObject;
-
 		private final int limit;
-
 		private final long skip;
 		private final @Nullable Class<?> type;
 
