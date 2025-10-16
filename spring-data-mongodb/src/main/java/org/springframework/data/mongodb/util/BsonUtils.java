dfe16bd240e4c7d40761b50861d27ba0d5d240a6
Christoph Strobl <christoph.strobl@broadcom.com>
2025-10-16 16:35:27 +0200

    Return early for Date to BsonDateTime conversion
    Add missing return statement to return value early instead of falling back to Codec based conversion.
    
    Closes: #5072

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/util/BsonUtils.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/util/BsonUtils.java
@@ -344,7 +344,7 @@ public class BsonUtils {
 	 * @throws IllegalArgumentException if {@literal source} does not correspond to a {@link BsonValue} type.
 	 * @since 4.2
 	 */
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Contract("null, _ -> !null")
 	public static BsonValue simpleToBsonValue(@Nullable Object source, CodecRegistry codecRegistry) {
 
@@ -393,7 +393,7 @@ public class BsonUtils {
 		}
 
 		if (source instanceof Date date) {
-			new BsonDateTime(date.getTime());
+			return new BsonDateTime(date.getTime());
 		}
 
 		try {
