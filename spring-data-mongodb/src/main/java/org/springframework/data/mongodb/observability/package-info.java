0e1ff168d6de79805d851b8899106418fcddae5f
Christoph Strobl <christoph.strobl@broadcom.com>
2025-10-23 09:51:51 +0200

    Update observability configuration section.
    Recommend using driver native API for observability.
    
    See #5009
    See #5078
    Original pull request #5080

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/observability/package-info.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/observability/package-info.java
@@ -1,5 +1,8 @@
 /**
  * Infrastructure to provide driver observability using Micrometer.
+ * <p>
+ * <strong>NOTE:</strong> MongoDB Java Driver 5.7+ comes with observability directly built in which can be configured
+ * via {@code MongoClientSettings.Builder#observabilitySettings(ObservabilitySettings)}.
  */
 @org.jspecify.annotations.NullMarked
 package org.springframework.data.mongodb.observability;
